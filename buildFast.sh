727961d11b605326c668e64dff0500a8d8a7a033
Ronie Salgado <roniesalg@gmail.com>
2023-03-25 11:28:03 -0300

    Do not forget to re-enable the on-demand bytecode compilation.

--- buildFast.sh
+++ buildFast.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -ex
-OUT_DIR="build/tuuvm-fast"
+OUT_DIR="build-fast"
 
 mkdir -p $OUT_DIR
 gcc -Wall -Wextra -Iinclude -I. -o $OUT_DIR/tuuvmi -g -O3 apps/interpreter/interpreter.c lib/tuuvm/unityBuild.c -lm
+
