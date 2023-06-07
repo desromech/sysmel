71484d13fbae744aa208662852615bdc1b300006
Ronie Salgado <roniesalg@gmail.com>
2023-06-07 03:24:00 -0400

    I am reducing the undefined primitives.

--- lib/sysbvm/errors.c
+++ lib/sysbvm/errors.c
@@ -85,6 +85,11 @@ SYSBVM_API void sysbvm_error_argumentCountMismatch(size_t expected, size_t gotte
     sysbvm_error("Argument count mismatch");
 }
 
+SYSBVM_API void sysbvm_error_primitiveFailed(void)
+{
+    sysbvm_error("Primitive failed");
+}
+
 static sysbvm_tuple_t sysbvm_errors_primitive_error(sysbvm_context_t *context, sysbvm_tuple_t closure, size_t argumentCount, sysbvm_tuple_t *arguments)
 {
     (void)context;
@@ -95,12 +100,25 @@ static sysbvm_tuple_t sysbvm_errors_primitive_error(sysbvm_context_t *context, s
     return SYSBVM_VOID_TUPLE;
 }
 
+static sysbvm_tuple_t sysbvm_errors_primitive_primitiveFailedError(sysbvm_context_t *context, sysbvm_tuple_t closure, size_t argumentCount, sysbvm_tuple_t *arguments)
+{
+    (void)context;
+    (void)closure;
+    (void)arguments;
+    if(argumentCount != 0) sysbvm_error_argumentCountMismatch(0, argumentCount);
+
+    sysbvm_error_primitiveFailed();
+    return SYSBVM_VOID_TUPLE;
+}
+
 void sysbvm_errors_registerPrimitives(void)
 {
     sysbvm_primitiveTable_registerFunction(sysbvm_errors_primitive_error, "Error::signalWithMessage");
+    sysbvm_primitiveTable_registerFunction(sysbvm_errors_primitive_primitiveFailedError, "Error::primitiveFailed");
 }
 
 void sysbvm_errors_setupPrimitives(sysbvm_context_t *context)
 {
     sysbvm_context_setIntrinsicSymbolBindingValueWithPrimitiveFunction(context, "error", 1, SYSBVM_FUNCTION_FLAGS_CORE_PRIMITIVE, NULL, sysbvm_errors_primitive_error);
+    sysbvm_context_setIntrinsicSymbolBindingValueWithPrimitiveFunction(context, "primitiveFailedError", 1, SYSBVM_FUNCTION_FLAGS_CORE_PRIMITIVE, NULL, sysbvm_errors_primitive_primitiveFailedError);
 }
