c4e90994bb6b4e2a7f7b4d84fa4f590b61c85480
Ronie Salgado <roniesalg@gmail.com>
2023-10-13 04:47:16 -0300

    Do not build the 32 bits version support libraries using SDL2.

--- scripts/build/buildSupport.sh
+++ scripts/build/buildSupport.sh
@@ -4,4 +4,4 @@ set -ex
 . $(dirname $(readlink -f "$0"))/buildCommon.sh
 
 $CC -pthread -Wall -Wextra -Iinclude -I. -DNDEBUG $BUILD_CFLAGS -g -c -o$OUT_DIR/sysmel-pal.o -O2 -ffunction-sections -fdata-sections lib/support/support.c
-$CC -m32 -pthread -Wall -Wextra -Iinclude -I. -DNDEBUG $BUILD_CFLAGS -g -c -o$OUT_DIR32/sysmel-pal.o -O2 -ffunction-sections -fdata-sections lib/support/support.c
+$CC -m32 -pthread -Wall -Wextra -Iinclude -I. -DNDEBUG -g -c -o$OUT_DIR32/sysmel-pal.o -O2 -ffunction-sections -fdata-sections lib/support/support.c
