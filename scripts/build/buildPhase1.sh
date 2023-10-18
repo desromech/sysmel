cd82b57f10c365042edaef1ec76d73f73274c47f
Ronie Salgado <roniesalg@gmail.com>
2023-10-18 04:39:30 -0300

    I am starting to separate the package loading onto different phases.

--- scripts/build/buildPhase1.sh
+++ scripts/build/buildPhase1.sh
@@ -3,5 +3,6 @@ set -ex
 
 . $(dirname $(readlink -f "$0"))/buildCommon.sh
 
-$OUT_DIR/phase0 -e "CLISysmelInterpreter buildNativeInterpreterImage saveTo: \"$OUT_DIR/phase1.o\""
+##$OUT_DIR/phase0 package-sources/EnvironmentPackages.sysmel -save-obj-file "$OUT_DIR/phase1.o"
+$OUT_DIR/phase0 -save-obj-file "$OUT_DIR/phase1.o"
 $CC -o $OUT_DIR/phase1 $OUT_DIR/phase1.o $OUT_DIR/sysmel-pal.o $BUILD_LD_FLAGS
